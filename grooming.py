4938a8c8ef405c877c0ba036054bab661c3200e8
Ed <oflynned@tcd.ie>
2017-08-18 23:09:43 +0100

    added more methods for better experimentation of data grooming for the app and for ML

--- grooming.py
+++ grooming.py
@@ -1,6 +1,7 @@
 import xml.etree.ElementTree as et
 import re
 import json
+import copy
 
 from string import digits
 
@@ -8,25 +9,42 @@ from string import digits
 class Grooming:
     @staticmethod
     def generate_modified_files():
-        with open("input_copy.xml", "r") as f:
+        with open("data/input_copy.xml", "r") as f:
             data = f.read().replace("xml:lang", "lang")
 
-        with open("xml_modified.xml", "w") as f:
+        with open("data/xml_modified.xml", "w") as f:
             f.write(data)
 
     @staticmethod
     def groom_xml_data_app():
-        output = []
+        groomed_terms = []
 
-        tree = et.parse("xml_modified.xml")
+        tree = et.parse("data/xml_modified.xml")
         elements = tree.getroot()
         for element in elements.iter("termEntry"):
             en_translations = []
+            domains = []
+            examples = []
             ga_term = ""
             gender = ""
+            declension = -1
 
             is_noun = False
             should_add_to_output = False
+
+            for domain in element.iter("descrip"):
+                if domain.attrib["type"] == "domain" and domain.attrib["lang"] == "en":
+                    curated_domains = str(domain.text).split("›")[0].split(",")
+                    for curation in curated_domains:
+                        domains.append(curation.strip())
+
+            # prevent duplications of domains
+            temp_domains = []
+            for domain in domains:
+                if domain not in temp_domains:
+                    temp_domains.append(domain)
+
+            domains = temp_domains
 
             for termEntry in element.iter("langSet"):
                 if termEntry.attrib["lang"] == "en":
@@ -50,25 +68,86 @@ class Grooming:
                             # plurals are bad, avoid them as much as possible
                             if str(ga_term[:3]) != "na ":
                                 if term_note is not None:
-                                    if term_note.text in gender_selection:
-                                        gender = gender_selection[term_note.text.translate({ord(k): None for k in digits})]
-                                        if re.findall(r'\d+', term_note.text) is not None:
-                                            # long phrases don't add anything in comparison to short phrases
-                                            # we should probably assume 5 words are enough
-                                            should_add_to_output = phrase_size < 5
-                                            break
+                                    gender = ''.join([i for i in term_note.text if not i.isdigit()])
+                                    declension = ''.join(c for c in term_note.text if c.isdigit())
+                                    has_declension = len(declension) > 0
 
-                if should_add_to_output:
-                    output.append({"ga": ga_term, "en": en_translations, "gender": gender})
+                                    if len(domains) > 0 and has_declension and gender in gender_selection:
+                                        gender = gender_selection[gender]
+                                        # long phrases don't add anything in comparison to short phrases
+                                        # we should probably assume 1 word is enough for vocab learning
+                                        WORD_SIZE_LIMIT = 2
+                                        should_add_to_output = phrase_size < WORD_SIZE_LIMIT
+                                        break
 
-        for item in output:
+            if should_add_to_output:
+                groomed_terms.append({
+                    "ga": ga_term, "en": en_translations,
+                    "gender": gender, "declension": int(declension),
+                    "domains": domains
+                })
+
+        # sort list by ga term alphabetically
+        output = []
+        groomed_terms = sorted(groomed_terms, key=lambda k: k["ga"], reverse=False)
+        for item in groomed_terms:
+            for domain in item["domains"]:
+                temp_item = copy.deepcopy(item)
+                temp_item.pop("domains", None)
+                temp_item["domain"] = domain
+                output.append(item)
+
+        for item in groomed_terms:
             print(item)
 
-        # 15,286 words
-        print(len(output), "items generated")
+        # ~44,510 words
+        print(len(groomed_terms), "items generated")
 
-        with open("output_app_nouns.json", "w") as f:
-            f.write(json.dumps(output, indent=4, ensure_ascii=False))
+        with open("data/output_app_nouns.json", "w") as f:
+            f.write(json.dumps(groomed_terms, indent=4, ensure_ascii=False))
+
+    @staticmethod
+    def split_app_nouns_files():
+        with open("data/output_app_nouns.json", "r") as f:
+            data = json.loads(f.read())
+
+        output = {}
+        domains = []
+
+        for term in data:
+            for domain in term["domains"]:
+                if domain not in domains and str(domain) is not "":
+                    domains.append(domain)
+
+        for item in domains:
+            output[item] = []
+
+        for term in data:
+            for domain in term["domains"]:
+                if str(domain) is not "":
+                    temp_item = term
+                    temp_item.pop("domains", None)
+                    output[domain].append(temp_item)
+
+        output = [output]
+
+        with open("data/output_domained_nouns.json", "w") as f:
+            f.write(json.dumps(output, indent=4, sort_keys=True, ensure_ascii=False))
+
+        # first save the master list of the domains as its own json to refer to later in the app
+        with open("data/domains.json", "w") as f:
+            f.write(json.dumps(domains, indent=4, sort_keys=True, ensure_ascii=False))
+
+        # now split each list element into its own file for individual loading later
+        for item in output:
+            for i, key in enumerate(item):
+                with open("data/nouns/" + Grooming.format_file_name(key) + ".json", "w") as f:
+                    data_to_save = sorted(item[key], key=lambda k: k["ga"], reverse=False)
+                    f.write(json.dumps(data_to_save, indent=4, sort_keys=True, ensure_ascii=False))
+
+    @staticmethod
+    def format_file_name(raw_string):
+        return str(raw_string).lower().replace("&", "and").replace(" ", "_")
 
     @staticmethod
     def groom_xml_data():
--- main.py
+++ main.py
@@ -1,10 +1,13 @@
 from grooming import Grooming
 
+
 def main():
     Grooming.groom_xml_data_app()
+    Grooming.split_app_nouns_files()
 
-    # ML.train_noun_data()
+    # ML.train_noun_data()
     # ML.run_network()
+
 
 if __name__ == "__main__":
     main()
